68e95d35f5ad73c3a2592bf5190311cc60ba9f62
Matt Fetterman <navx2810@gmail.com>
2017-10-16 19:49:37 -0400

    initial commit

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Test.Models;
+using MarketCarpenterCore.Ext.MojoTemplateEngine;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index([FromServices]IEngine engine)
+        {
+            string val = engine.Compile("a.hbs").Render(new { name = "matt" });
+            return Ok(val);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
